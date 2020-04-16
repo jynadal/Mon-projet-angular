f551e4c1dd792a0298080d96123394a248a789c7
Jean_Yves <nadaljeanyves@hotmail.fr>
2020-04-16 16:25:25 +0200

    property binding

--- src/app/appareil/appareil.component.ts
+++ src/app/appareil/appareil.component.ts
@@ -7,9 +7,16 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AppareilComponent implements OnInit {
 
+  appareilName: string = "Machine à laver";
+  appareilStatus: string = 'éteint';
+
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  getStatus() {
+    return this.appareilStatus;
   }
 
 }
