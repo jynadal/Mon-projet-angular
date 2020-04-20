dd01749d7bbc6493d54c6157283ef24d322b7353
Jean-Yves <nadaljeanyves@hotmail.fr>
2020-04-20 13:36:21 +0200

    Suite routing avant 404

--- src/app/services/appareil.service.ts
+++ src/app/services/appareil.service.ts
@@ -1,18 +1,31 @@
 export class AppareilService {
     appareils = [
         {
+          id: 1,
           name: 'Machine à  laver',
           status: 'éteint'
         },
         {
+          id: 2,
           name: 'Frigo',
           status: 'allumé'
         },
         {
+          id: 3,
           name: 'Ordinateur',
           status: 'allumé'
         }
       ];
+
+      
+      getAppareilById(id: number) {
+        const appareil = this.appareils.find(
+          (appareilObject) => {
+            return appareilObject.id === id;
+          }
+        );
+        return appareil;
+      }
 
       switchOnAll() {
           for(let appareil of this.appareils) {
@@ -32,5 +45,6 @@ export class AppareilService {
       switchOffOne(i: number) {
           this.appareils[i].status = 'éteint';
       }
+
 
 }
