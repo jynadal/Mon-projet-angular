1ec5d85f5c9949a2a3710531b6e5f5eb3dc69543
Jean-Yves <nadaljeanyves@hotmail.fr>
2020-04-21 18:10:21 +0200

    Fin de RxJS et du chapitre 2

--- src/app/appareil-view/appareil-view.component.ts
+++ src/app/appareil-view/appareil-view.component.ts
@@ -1,15 +1,18 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 import { AppareilService } from '../services/appareil.service';
+import { Subscription } from 'rxjs/Subscription';
+//-compat
 
 @Component({
   selector: 'app-appareil-view',
   templateUrl: './appareil-view.component.html',
   styleUrls: ['./appareil-view.component.scss']
 })
-export class AppareilViewComponent implements OnInit {
+export class AppareilViewComponent implements OnInit, OnDestroy {
 
-
- isAuth = false;
+ //isAuth = false;
+ appareils: any[];
+ appareilSubscription: Subscription;
 
   lastUpdate = new Promise((resolve, reject) => {
     const date = new Date();
@@ -20,23 +23,23 @@ export class AppareilViewComponent implements OnInit {
       );
      });
 
-appareils: any[];
   
   //Button
-  constructor(private appareilService: AppareilService) {
-    setTimeout(
-      () => {
-        this.isAuth=true;
-      }, 4000
-    );
-  }
+  constructor(private appareilService: AppareilService) { }
 
   ngOnInit() {
-    this.appareils = this.appareilService.appareils;    
+    this.appareilSubscription =
+     this.appareilService.appareilsSubject.subscribe(
+     (appareils: any[]) => {
+       this.appareils = appareils;
+     }
+     );
+     this.appareilService.emitAppareilSubject();    
   }
 
   onAllumer() {
     this.appareilService.switchOnAll();
+    
   }
   onEteindre() {
     if(confirm('Etes-vous sûr de vouloir éteindre tous vos appareils ?')) {
@@ -45,5 +48,9 @@ appareils: any[];
       return null;
     }
     
+  }
+
+  ngOnDestroy() {
+    this.appareilSubscription.unsubscribe();
   }
 }
--- src/app/services/appareil.service.ts
+++ src/app/services/appareil.service.ts
@@ -1,5 +1,9 @@
+import { Subject } from 'rxjs/Subject';
+//compat
 export class AppareilService {
-    appareils = [
+  appareilsSubject = new Subject<any[]>();
+
+    private appareils = [
         {
           id: 1,
           name: 'Machine à  laver',
@@ -27,23 +31,32 @@ export class AppareilService {
         return appareil;
       }
 
+      emitAppareilSubject() {
+        this.appareilsSubject.next(this.appareils.slice());
+      }
+
       switchOnAll() {
           for(let appareil of this.appareils) {
               appareil.status = 'allumé';
           }
+          this.emitAppareilSubject();
       }
 
       switchOffAll() {
           for(let appareil of this.appareils) {
               appareil.status = 'éteint';
+              this.emitAppareilSubject();
           }
+        
       }
 
       switchOnOne(i: number) {
           this.appareils[i].status = 'allumé';
+          this.emitAppareilSubject();
       }
       switchOffOne(i: number) {
           this.appareils[i].status = 'éteint';
+          this.emitAppareilSubject();
       }
 
 
