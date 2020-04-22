17993cdc86ff8b6ba0f1fad25fe9dc5716f24fd1
Jean-Yves <nadaljeanyves@hotmail.fr>
2020-04-22 18:19:12 +0200

    Fin reativeform avant validation

--- src/app/new-user/new-user.component.ts
+++ src/app/new-user/new-user.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup } from '@angular/forms';
+import { UserService } from '../services/user.service';
+import { Router } from '@angular/router';
+import { User } from '../models/User.model';
 
 @Component({
   selector: 'app-new-user',
@@ -7,9 +11,34 @@ import { Component, OnInit } from '@angular/core';
 })
 export class NewUserComponent implements OnInit {
 
-  constructor() { }
+  userForm: FormGroup;
+
+  constructor(private formBuilder: FormBuilder,
+              private userService: UserService,
+              private router: Router) { }
 
   ngOnInit(): void {
+    this.initForm();
+  }
+  initForm() {
+    this.userForm = this.formBuilder.group({
+      firstName:'',
+      lastName:'',
+      email:'',
+      drinkPreference:''
+    });
   }
 
+
+  onSubmitForm() {
+    const formValue = this.userForm.value;
+    const newUser = new User(
+      formValue['firstName'],
+      formValue['lastName'],
+      formValue['email'],
+      formValue['drinkPreference']
+    );
+    this.userService.addUser(newUser);
+    this.router.navigate(['/users']);
+  }
 }
